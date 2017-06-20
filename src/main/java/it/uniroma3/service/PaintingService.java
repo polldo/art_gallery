2b69c73bbecbb5d7a3d6f4ba041cfb059335644f
Poldo <paolo.calao@gmail.com>
2017-06-20 16:19:07 +0200

    -added webjars dependency

--- src/main/java/it/uniroma3/service/PaintingService.java
+++ src/main/java/it/uniroma3/service/PaintingService.java
@@ -2,26 +2,30 @@ package it.uniroma3.service;
 
 import it.uniroma3.model.Painting;
 import it.uniroma3.repository.PaintingRepository;
+import it.uniroma3.repository.PictureRepository;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class PaintingService {
 	private final PaintingRepository paintingRepository;
+	private final PictureRepository pictureRepository;
 	private final Logger logger = Logger.getLogger(PaintingService.class);
-	
+
 	@Autowired
-	public PaintingService(PaintingRepository paintingRepository) {
+	public PaintingService(PaintingRepository paintingRepository, PictureRepository pictureRepository) {
 		this.paintingRepository = paintingRepository;
+		this.pictureRepository = pictureRepository;
 	}
-	
+
 	public List<Painting> getAll() {
 		logger.info("Get all Paintings");
 		return paintingRepository.findAll();
 	}
-	
+
 	public Painting getPainting(Long id) {
 		logger.info("Get Painting by id " + id);
 		return paintingRepository.findOne(id);
@@ -35,13 +39,13 @@ public class PaintingService {
 	public List<Painting> getPaintingByMedium(String medium) {
 		logger.info("Get Painting by medium " + medium);
 		return paintingRepository.findPaintingsByMedium(medium);
-	}	
-	
+	}
+
 	public List<Painting> getPaintingByYear(Integer year) {
 		logger.info("Get Painting by year " + year);
 		return paintingRepository.findPaintingsByYear(year);
 	}
-	
+
 	public List<Painting> getPaintingByRoom(Long id) {
 		logger.info("Get Painting by Room " + id);
 		return paintingRepository.findPaintingsByRoom_Id(id);
@@ -64,6 +68,10 @@ public class PaintingService {
 
 	public void removePaintingsByAuthorId(Long id) {
 		logger.info("Remove Paintings by Author " + id);
+		List<Painting> paintings = getPaintingByAuthorId(id);
+		for (Painting paint : paintings) {
+			pictureRepository.deletePicturesByPainting_Id(paint.getId());
+		}
 		paintingRepository.deletePaintingsByAuthor_Id(id);
 	}
 }
